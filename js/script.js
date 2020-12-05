e12694141335e7a44edeb781cc3a263649c55ef1
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-05 11:16:29 -0600

    Contiene un buscador de Heroess

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@
 //laves para el acceso a la API
 const privateKey = "f3cd8da61a4e04a5934911f54a900a148dc0aac9",
       publicKey =  "070319a50d29b224b7fff55c1d225937",
-      content = document.getElementById('content');
+      content = document.getElementById('content'),
+      //Trayendo la entrada cogida en el teclado
+      search =  document.getElementById('search'); // funcion debajo
 
       //creando metod para usar el FETCH que tarera ls datos
 const getConnection= () =>{
@@ -27,8 +29,9 @@ fetch(URL).then(response => response.json() ). // MUESTRA EN FORMATO json LA res
                  
             response.data.results.forEach( e => {
                         drawHero(e);            })
-           );
-        } 
+           ).catch(e => console.log(e));    // el catch sirve para validar un errror
+
+        }; 
 
         // Pintara en pantalla la imagen de cada super heroe
 const drawHero = e =>{
@@ -50,6 +53,33 @@ content.insertAdjacentHTML('beforeEnd', hero);
 
 };
 
+
+const searchHero = name => {
+
+    //formatear la entrada
+    hero = encodeURIComponent(name)
+
+    const ts = Date.now();
+    const hash = md5(ts+privateKey+publicKey),
+     // esta es la api de marvel
+    URL =`http://gateway.marvel.com/v1/public/characters?name=${hero}&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    
+    //haciendo la peticion a  AJAX
+    //imprime por consola la respuesta del RESPONSE
+    fetch(URL).then(response => response.json() ). // MUESTRA EN FORMATO json LA respuesta de la API
+               then(response => 
+                     response.data.results.forEach(e=> {drawHero(e)})
+                   ).catch(e => console.log(e));    // el catch sirve para validar un errror
+    }
+
+search.addEventListener('keyup', e=> {
+
+    if(e.keyCode ===13){
+           content.innerHTML = '';    //elimina todo el contenido de la ventana ates de mostrar el hero buscado
+           searchHero(e.target.value.trim())
+                       }
+
+})
 
  getConnection();                          
 
